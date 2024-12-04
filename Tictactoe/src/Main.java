6717c0e3408fc78961e6d18762869ca9cb660ded
neshafwana30 <5026231013@student.its.ac.id>
2024-12-04 21:31:38 +0700

    Delete Tictactoe directory

--- Tictactoe/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Playing Tic Tac Toe");
-        TTT game = new TTT();
-        game.play();
-    }
-}
