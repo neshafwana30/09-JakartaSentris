c0a5cf9f724cf03d147b084eb4381a722ba15b8c
keishaadisti <162418632+keishaadisti@users.noreply.github.com>
2024-12-06 23:27:31 +0700

    Cell.java

--- Cell.java
+++ Cell.java
@@ -1,15 +1,1 @@
-public class Cell {
-    private int value;
 
-    public Cell() {
-        this.value = 0; // Default value is empty
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    public void setValue(int value) {
-        this.value = value;
-    }
-}
