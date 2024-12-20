c60b5b1e6744a9ac1ebde9ea8a4be28ab2b498c5
keishaadisti <162418632+keishaadisti@users.noreply.github.com>
2024-12-20 19:20:03 +0700

    Add files via upload
    AudioPlayer class for Sudoku package

--- AudioPlayer.java
+++ AudioPlayer.java
@@ -1,4 +1,5 @@
-=======
+package Sudoku;
+
 import java.io.File;
 import java.io.IOException;
 import javax.sound.sampled.AudioInputStream;
@@ -11,23 +12,17 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 public class AudioPlayer {
     private static Clip clip;
 
-    // Method untuk memutar audio
     public static void playAudio(String filePath) {
         try {
-            // Lokasi file audio
             File audioFile = new File(filePath);
 
-            // Membuat AudioInputStream
             try (AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile)) {
 
-                // Membuat Clip untuk memainkan suara
                 clip = AudioSystem.getClip();
                 clip.open(audioStream);
 
-                // Memulai pemutaran audio
                 clip.start();
 
-                // Tunggu hingga pemutaran selesai
                 clip.addLineListener(event -> {
                     if (event.getType() == LineEvent.Type.STOP) {
                         clip.close();
@@ -46,7 +41,7 @@ public class AudioPlayer {
         }
     }
 
-    // Method untuk memutar audio di latar belakang
     public static void playAudioInBackground(String filePath) {
         new Thread(() -> playAudio(filePath)).start();
+    }
 }
